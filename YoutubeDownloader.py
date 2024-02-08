8322cac7e2e7686f85085eba32356ea294ee1e0f
LexTutor <filatyev.a.a@gmail.com>
2024-02-08 17:40:33 +0300

    Изменения сохранения файлов из плейлиста теперь добавляется порядковый номер в плейлисте к имени файла

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,19 +1,11 @@
 # подключаем библиотеки
-from tkinter import *
+
 import pytube
-from tkinter import messagebox
-
-# рисуем главное окно
-root = Tk()
-root.geometry("500x250")
-root.resizable(False, False)
-# заголовок окна
-root.title("Код")
-# цвет фона
-root.config(bg='#D3D3D3')
+from tkinter import messagebox, Tk, Label, StringVar, Entry, Button
 
 
-# механика кнопки Скачать
+#
+# # механика кнопки Скачать
 def download():
     # Проверяем адрес видео если не пустой пытаемся качать
     ytlink = link_video.get()
@@ -28,15 +20,22 @@ def download():
 
 
 def download_playlist(url):
+    """
+    Функция скачивания всех видео из плейлиста на YouTube
+    :param url: ссылка плейлиста на YouTube:
+    :return:
+    """
     try:
         playlist = pytube.Playlist(url)
         # print('Number of videos in playlist: %s' % len(playlist.video_urls))
         # playlist.download_all()
-        for video in playlist.videos:
+        videos = list(playlist.videos)
+        for idx in range(0, len(videos)):
             # получаем ссылку на видео с самым высоким качеством
-            video = video.streams.get_highest_resolution()
+            video = videos[idx].streams.get_highest_resolution()
+            new_file_name = str(idx+1) + "-" + video.default_filename
             # скачиваем видео
-            video.download()
+            video.download(filename=new_file_name)
         messagebox.showinfo("Готово", "Загрузка завершена")
     except:
         # выводим сообщение об ошибке
@@ -44,6 +43,11 @@ def download_playlist(url):
 
 
 def download_video(url):
+    """
+    Функция скачивания одного видео с YouTube
+    :param url: ссылка на видео на YouTube:
+    :return:
+    """
     # пробуем скачать видео по ссылке
     try:
         # переводим его в нужный формат
@@ -66,16 +70,24 @@ def reset():
 
 
 # при нажатии на кнопку выхода — закрываем окно с интерфейсом
-def quit():
+def quiting():
     root.destroy()
 
 
+# рисуем главное окно
+root = Tk()
+root.geometry("800x500")
+root.resizable(False, False)
+# заголовок окна
+root.title("Код")
+# цвет фона
+root.config(bg='#D3D3D3')
 # заголовок формы
 lb = Label(root, text="---Загрузка видео с YouTube---", font=('Arial,15,bold'), bg='#D3D3D3')
 lb.pack(pady=15)
 # пояснительный текст для поля с адресом
 lb1 = Label(root, text="Ссылка на видео :", font=('Arial,15,bold'), bg='#D3D3D3')
-lb1.place(x=10, y=80)
+lb1.place(x=10, y=60)
 
 # пояснительный текст для поля с адресом плэйлиста
 lb2 = Label(root, text="Ссылка на PlayList :", font=('Arial,15,bold'), bg='#D3D3D3')
@@ -83,13 +95,13 @@ lb2.place(x=10, y=100)
 
 # поле ввода адреса видео
 link_video = StringVar()
-En1 = Entry(root, textvariable=link_video, font=('Arial,15,bold'))
-En1.place(x=230, y=80)
+en1 = Entry(root, textvariable=link_video, font=('Arial,15,bold'))
+en1.place(x=230, y=60)
 
 # поле ввода адреса видео
 link_playlist = StringVar()
-En1 = Entry(root, textvariable=link_playlist, font=('Arial,15,bold'))
-En1.place(x=230, y=100)
+en1 = Entry(root, textvariable=link_playlist, font=('Arial,15,bold'))
+en1.place(x=230, y=100)
 
 # кнопка скачивания
 btn1 = Button(root, text="Скачать", font=('Arial,10,bold'), bd=4, command=download)
@@ -98,8 +110,8 @@ btn1.place(x=330, y=130)
 # кнопки очистки и выхода
 btn2 = Button(root, text="Очистить", font=('Arial,10,bold'), bd=4, command=reset)
 btn2.place(x=120, y=190)
-btn3 = Button(root, text=" Выход ", font=('Arial,10,bold'), bd=4, command=quit)
+btn3 = Button(root, text=" Выход ", font=('Arial,10,bold'), bd=4, command=quiting)
 btn3.place(x=250, y=190)
-
+print(root.children)
 # запускаем окно
 root.mainloop()
