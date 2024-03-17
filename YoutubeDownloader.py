dac28c6ecbcf074c5e34107344bb939e67b5c6c3
lex <filatyev.a.a@gmail.com>
2024-03-17 09:27:56 +0300

    изменение 1

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -3,8 +3,9 @@
 import pytube
 from tkinter import messagebox, Tk, Label, StringVar, Entry, Button
 
+ERROR_TITLE = "Ошибка"
 
-#
+
 # # механика кнопки Скачать
 def download():
     # Проверяем адрес видео если не пустой пытаемся качать
@@ -16,7 +17,7 @@ def download():
         if len(ytplink) > 0:
             download_playlist(ytplink)
     else:
-        messagebox.showerror("Ошибка", "Ни одно поле с адресом незаполнено")
+        messagebox.showerror(ERROR_TITLE, "Ни одно поле с адресом незаполнено")
 
 
 def download_playlist(url):
@@ -27,19 +28,17 @@ def download_playlist(url):
     """
     try:
         playlist = pytube.Playlist(url)
-        # print('Number of videos in playlist: %s' % len(playlist.video_urls))
-        # playlist.download_all()
         videos = list(playlist.videos)
         for idx in range(0, len(videos)):
             # получаем ссылку на видео с самым высоким качеством
             video = videos[idx].streams.get_highest_resolution()
-            new_file_name = str(idx+1) + "-" + video.default_filename
+            new_file_name = str(idx + 1) + "-" + video.default_filename
             # скачиваем видео
             video.download(filename=new_file_name)
         messagebox.showinfo("Готово", "Загрузка завершена")
     except:
         # выводим сообщение об ошибке
-        messagebox.showerror("Ошибка", "Шота пашло не так")
+        messagebox.showerror(ERROR_TITLE, "Шота пашло не так")
 
 
 def download_video(url):
@@ -61,7 +60,7 @@ def download_video(url):
     # если скачать не получилось
     except:
         # выводим сообщение об ошибке
-        messagebox.showerror("Ошибка", "Ссылка не работает")
+        messagebox.showerror(ERROR_TITLE, "Ссылка не работает")
 
 
 # при нажатии на кнопку очистки очищаем строку с адресом видео
@@ -77,41 +76,36 @@ def quiting():
 
 # рисуем главное окно
 root = Tk()
-root.geometry("800x500")
+root.geometry("720x350")
 root.resizable(False, False)
 # заголовок окна
 root.title("Код")
 # цвет фона
 root.config(bg='#D3D3D3')
 # заголовок формы
-lb = Label(root, text="---Загрузка видео с YouTube---", font=('Arial,15,bold'), bg='#D3D3D3')
+lb = Label(root, text="---Загрузка видео с YouTube---", font=('Arial', 10, 'bold'), bg='#D3D3D3')
 lb.pack(pady=15)
 # пояснительный текст для поля с адресом
-lb1 = Label(root, text="Ссылка на видео :", font=('Arial,15,bold'), bg='#D3D3D3')
+lb1 = Label(root, text="Ссылка на видео :", font=('Arial', 10, 'bold'), bg='#D3D3D3')
 lb1.place(x=10, y=60)
-
 # пояснительный текст для поля с адресом плэйлиста
-lb2 = Label(root, text="Ссылка на PlayList :", font=('Arial,15,bold'), bg='#D3D3D3')
+lb2 = Label(root, text="Ссылка на PlayList :", font=('Arial', 10, 'bold'), bg='#D3D3D3')
 lb2.place(x=10, y=100)
-
 # поле ввода адреса видео
 link_video = StringVar()
-en1 = Entry(root, textvariable=link_video, font=('Arial,15,bold'))
-en1.place(x=230, y=60)
-
+en1 = Entry(root, textvariable=link_video, font=('Arial', 10, 'bold'), width=75)
+en1.place(x=150, y=60)
 # поле ввода адреса видео
 link_playlist = StringVar()
-en1 = Entry(root, textvariable=link_playlist, font=('Arial,15,bold'))
-en1.place(x=230, y=100)
-
+en1 = Entry(root, textvariable=link_playlist, font=('Arial', 10, 'bold'), width=75)
+en1.place(x=150, y=100)
 # кнопка скачивания
-btn1 = Button(root, text="Скачать", font=('Arial,10,bold'), bd=4, command=download)
-btn1.place(x=330, y=130)
-
+btn1 = Button(root, text="Скачать", font=('Arial', 10, 'bold'), bd=4, command=download)
+btn1.place(x=590, y=130)
 # кнопки очистки и выхода
-btn2 = Button(root, text="Очистить", font=('Arial,10,bold'), bd=4, command=reset)
-btn2.place(x=120, y=190)
-btn3 = Button(root, text=" Выход ", font=('Arial,10,bold'), bd=4, command=quiting)
+btn2 = Button(root, text="Очистить", font=('Arial', 10, 'bold'), bd=4, command=reset)
+btn2.place(x=450, y=130)
+btn3 = Button(root, text=" Выход ", font=('Arial', 10, 'bold'), bd=4, command=quiting)
 btn3.place(x=250, y=190)
 # запускаем окно
 root.mainloop()
